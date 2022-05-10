e4dc75c19e307b9c55a3986ba18ce92e67dfe4ab
Antonio José Tirado Pancorbo <antoniojose.tirado@bosonit.com>
2022-05-10 12:27:32 +0200

    SA1 Websocket Example

---
+++ SA1/src/main/java/com/backend/SA1/Greeting.java
@@ -0,0 +1,12 @@
+package com.backend.SA1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Greeting {
+    private String content;
+}
---
+++ SA1/src/main/java/com/backend/SA1/HelloMessage.java
@@ -0,0 +1,14 @@
+package com.backend.SA1;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class HelloMessage {
+    private String name;
+}
